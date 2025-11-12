06da0e54b419936cadbc60311b634b484bb4cdf7
Theo <30200282+atultw@users.noreply.github.com>
2025-11-12 05:15:00 +0000

    LLS

--- LLS.hpp
+++ LLS.hpp
@@ -11,17 +11,29 @@ private:
     LinkedList<T> list;
 public:
     // Constructor
-    LLS();
+    LLS() {
+        list = new LinkedList<T>();
+    };
 
     // Insertion
-    void push(const T& item) override;
+    void push(const T& item) override {
+        list->addTail(item);
+    };
 
     // Deletion
-    T pop() override;
+    T pop() override {
+        T val = list->getTail();
+        list->removeTail();
+        return val;
+    };
 
     // Access
-    T peek() const override;
+    T peek() const override {
+        return list->getTail();
+    };
 
     //Getters
-    std::size_t getSize() const noexcept override;
+    std::size_t getSize() const noexcept override {
+        return list->getCount();
+    };
 };
