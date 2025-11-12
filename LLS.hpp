81d1e47f4604ca051ca46d3f3d2081af452fd41f
Theo <30200282+atultw@users.noreply.github.com>
2025-11-12 05:58:51 +0000

    fix

--- LLS.hpp
+++ LLS.hpp
@@ -22,6 +22,7 @@ public:
 
     // Deletion
     T pop() override {
+        if (getSize() == 0) throw std::runtime_error();
         T val = list.getTail()->data;
         list.removeTail();
         return val;
@@ -29,6 +30,7 @@ public:
 
     // Access
     T peek() const override {
+        if (getSize() == 0) throw std::runtime_error();
         return list.getTail()->data;
     };
 
