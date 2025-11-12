139f3abe4bcedf5740864d7f7bfe572f340a3ced
Theo <30200282+atultw@users.noreply.github.com>
2025-11-12 02:48:04 +0000

    fix

--- LinkedList.hpp
+++ LinkedList.hpp
@@ -3,15 +3,12 @@
 using namespace std;
 
 template <typename T>
+class LinkedList {
 struct Node {
     T data;
     Node* prev;
     Node* next;
 };
-
-template <typename T>
-class LinkedList {
-
 public:
 	// Behaviors
 	void printForward() const;
@@ -61,10 +58,10 @@ public:
 		if (head == nullptr) {
 			return false;
 		}
-		if (count == 1) clearInternal();
-		Node* oldptr = head;
-		head = head.next;
-		head->prev = nullptr;
+		if (this->count == 1) clearInternal();
+		Node* oldptr = this->head;
+		this->head = this->head.next;
+		this->head->prev = nullptr;
 		delete oldptr;
 		count--;
 		return true;
@@ -91,7 +88,7 @@ public:
 
 	// Operators
 	LinkedList<T>& operator=(LinkedList<T>&& other) noexcept {
-		if (this == &other) { return *this };
+		if (this == &other) { return *this; }
 
 		this->Clear();
 
@@ -106,7 +103,7 @@ public:
 		return *this;
 	};
 	LinkedList<T>& operator=(const LinkedList<T>& rhs) {
-		if (this == &rhs) { return *this };
+		if (this == &rhs) { return *this; }
 		
 		Clear();
 
@@ -138,9 +135,9 @@ public:
 
 	};
 	LinkedList(LinkedList<T>&& other) noexcept {
-		count = other.count;
-		head = other.head;
-		tail = other.tail;
+		this->count = other.count;
+		this->head = other.head;
+		this->tail = other.tail;
 
 		other.count = 0;
 		other.head = nullptr;
