dae9f606efe1c15c8b1e54f32a9b795fd12ba581
Theo <30200282+atultw@users.noreply.github.com>
2025-11-12 06:21:02 +0000

    fix

--- LLDQ.hpp
+++ LLDQ.hpp
@@ -15,22 +15,44 @@ private:
 
 public:
     // Constructor
-    LLDQ();
+    LLDQ() {
+        list = LinkedList<T>{};
+    };
 
     // Core Insertion Operations
-    void pushFront(const T& item) override;
-    void pushBack(const T& item) override;
+    void pushFront(const T& item) override {
+        list.addHead(item);
+    };
+    void pushBack(const T& item) override {
+        list.addTail(item);
+    };
 
     // Core Removal Operations
-    T popFront() override;
-    T popBack() override;
+    T popFront() override {
+        if (getSize() == 0) throw std::runtime_error("");
+        T val = list.getHead()->data;
+        list.removeHead();
+        return val;
+    };
+    T popBack() override {
+        if (getSize() == 0) throw std::runtime_error("");
+        T val = list.getTail()->data;
+        list.removeTail();
+        return val;
+    };
 
     // Element Accessors
-    const T& front() const override;
-    const T& back() const override;
+    const T& front() const override {
+        return list.getHead();
+    };
+    const T& back() const override {
+        return list.getTail();
+    };
 
     // Getter
-    std::size_t getSize() const noexcept override;
+    std::size_t getSize() const noexcept override {
+        return list.getCount();
+    };
 };
 
 
